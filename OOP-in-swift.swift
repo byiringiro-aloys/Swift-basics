384814203a5aad5ab53ece6002037f6761b12f67
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-11-14 10:13:00 +0200

    computed properties and will&didSet methods

--- OOP-in-swift.swift
+++ OOP-in-swift.swift
@@ -7,7 +7,7 @@ protocol Shape {
 }
 // fileprivate - file scope 
 // private - class scope or file scope
-internal class Rectangle: Shape{
+internal class Circle: Shape{
     fileprivate var radius: Double
     let PI=3.14
     init(radius: Double){
@@ -18,7 +18,36 @@ internal class Rectangle: Shape{
     }
 }
 
-var rectangle = Rectangle(radius: 3.5)
+struct Rectangle {
+    var width: Double
+    var height: Double
+    init(width: Double,height: Double){
+        self.width=width
+        self.height=height
+    }
+    // Computed property
+    var area: Double {
+        return width * height
+    }
+    
+    // Computed property with get & set
+    var perimeter: Double {
+        get {
+            return 2 * (width + height)
+        }
+        set(newPerimeter) {
+            width = newPerimeter / 4
+            height = newPerimeter / 4
+        }
+    }
+}
+var rect = Rectangle(width:10,height:20)
+print(rect.area)
+rect.perimeter=29
+print(rect.width)
+print(rect.height)
+
+var rectangle = Circle(radius: 3.5)
 print(rectangle.area())
 print(rectangle.radius)
 
@@ -134,3 +163,15 @@ handleNavigation(screen: homeSection)
 
 let settingSection = Screen.settings
 handleNavigation(screen: settingSection)
+
+var Score: Int = 1{
+    willSet(newScore){
+        print("The score will change to \(newScore).")
+    }
+
+    didSet{
+        print("The score changed from \(oldValue) to \(Score)")
+    }
+}
+
+Score = 10
